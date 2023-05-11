b8d00970ae13fe5d1c5074ec153009b60121f0d6
Chris K <Chris-SourceCode@outlook.com>
2023-05-11 21:52:04 +0200

    refactor: Change email and picture

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,11 +1,10 @@
-'use client'
+'use client';
 
-import react from 'react'
-import Card from './Card'
-import Image from 'next/image'
-import styles from './page.module.css'
-import GitHubIcon from '@mui/icons-material/GitHub'
-import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import Card from './Card';
+import Image from 'next/image';
+import styles from './page.module.css';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export default function Team() {
   return (
@@ -15,41 +14,41 @@ export default function Team() {
       </div>
       <div className={styles.allCards}>
         <Card
-          name="Bjorn Carlsson"
-          email="bacarlsson@gmail.com"
-          github="https://github.com/bacarlsson86"
-          linkedin=""
-          image="/beejorn.jpeg"
+          name='Bjorn Carlsson'
+          email='bacarlsson@gmail.com'
+          github='https://github.com/bacarlsson86'
+          linkedin=''
+          image='/beejorn.jpeg'
         />
         <Card
-          name="Jason Brown"
-          email="superbunker@gmail.com"
-          github="https://github.com/superbunker"
-          linkedin="https://www.linkedin.com/in/jasonbr/"
-          image="/jason.png"
+          name='Jason Brown'
+          email='superbunker@gmail.com'
+          github='https://github.com/superbunker'
+          linkedin='https://www.linkedin.com/in/jasonbr/'
+          image='/jason.png'
         />
         <Card
-          name="Ryan Gause"
-          email="ryan.g.gause.3@gmail.com"
-          github="https://github.com/Gauserr"
-          linkedin="https://www.linkedin.com/in/ryangause"
-          image="/ryang.png"
+          name='Ryan Gause'
+          email='ryan.g.gause.3@gmail.com'
+          github='https://github.com/Gauserr'
+          linkedin='https://www.linkedin.com/in/ryangause'
+          image='/ryang.png'
         />
         <Card
-          name="Michael Ruiz"
-          email="maruiz.sc1@gmail.com"
-          github="https://github.com/Ruizmichael"
-          linkedin="https://www.linkedin.com/in/michael-a-ruiz/"
-          image="/michael.png"
+          name='Michael Ruiz'
+          email='maruiz.sc1@gmail.com'
+          github='https://github.com/Ruizmichael'
+          linkedin='https://www.linkedin.com/in/michael-a-ruiz/'
+          image='/michael.png'
         />
         <Card
-          name="Chris Kulaczkowski"
-          email="krzysztof.kulaczkowski@hotmail.com"
-          github="https://github.com/Chris-isCoding"
-          linkedin="https://www.linkedin.com/in/chris-iscoding/"
-          image="/chris.png"
+          name='Chris Kulaczkowski'
+          email='chris@kulaczkowski.com'
+          github='https://github.com/Chris-isCoding'
+          linkedin='https://www.linkedin.com/in/Chris-isCoding/'
+          image='/chris.jpeg'
         />
       </div>
     </>
-  )
+  );
 }
