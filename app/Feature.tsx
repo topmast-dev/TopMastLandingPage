638d02e82c69441e89d3430a9ccbe3303721df9a
Chris K <Chris-SourceCode@outlook.com>
2023-05-11 21:46:52 +0200

    refactor: Remove redundant import

--- app/Feature.tsx
+++ app/Feature.tsx
@@ -1,11 +1,16 @@
-import react from 'react'
-import styles from './about/page.module.css'
+import styles from './about/page.module.css';
 
-export default function Feature({title, description}: {title:string, description:string}){
+export default function Feature({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) {
   return (
     <div className={styles.feature}>
       <h3>{title}</h3>
       <p>{description}</p>
     </div>
-  )
+  );
 }
